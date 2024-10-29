a9eb28d63d2beeb1a74870db69c0e60f7c83dca3
aurelien-bonin <aurelien.bonin@etu.minesparis.psl.eu>
2024-10-29 16:26:13 +0100

    woooop pi pooop

---
+++ .ipynb_checkpoints/common-checkpoint.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# fonction pour charger le contenu en excel
+
+def load_data(filename):
+    df = pd.read_excel(filename)
+    df = df.rename(columns={"foo": "bar"})
+    return df
+
