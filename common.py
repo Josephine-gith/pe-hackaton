cb16febb1a5be123e57c4457646dc49d44154b60
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2024-10-29 16:24:40 +0100

    macdonald

--- common.py
+++ common.py
@@ -6,3 +6,14 @@ def load_data(filename):
     df = pd.read_excel(filename)
     df = df.rename(columns={"foo": "bar"})
     return df
+
+def clean_data(colonne1, colonne2, colonne3 = None):
+    df_notna=(colonne1.notna())&(colonne2.notna())
+    return colonne1[df_notna], colonne2[df_notna]
+
+def tracage_regression(colonne1, colonne2, colonne3 = None):
+    plt.plot(social,healthy, '+')
+    regression = scipy.stats.linregress(colonne1,colonne2)
+    plt.plot(np.linspace(0.4,1,1000), regression[0]*np.linspace(0.4,1,1000)+ regression[1])
+    plt.xlabel(colonne1.name)
+    plt.ylabel(colonne2.name)
