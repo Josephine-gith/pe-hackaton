dabe45f86af70a73aeca14648ef5855eb5ec5209
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2024-10-29 16:24:58 +0100

    common update

--- common.py
+++ common.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+import scipy.stats
+import matplotlib.pyplot as plt
+import numpy as np
 # fonction pour charger le contenu en excel
 
 def load_data(filename):
@@ -12,7 +14,7 @@ def clean_data(colonne1, colonne2, colonne3 = None):
     return colonne1[df_notna], colonne2[df_notna]
 
 def tracage_regression(colonne1, colonne2, colonne3 = None):
-    plt.plot(social,healthy, '+')
+    plt.plot(colonne1,colonne2, '+')
     regression = scipy.stats.linregress(colonne1,colonne2)
     plt.plot(np.linspace(0.4,1,1000), regression[0]*np.linspace(0.4,1,1000)+ regression[1])
     plt.xlabel(colonne1.name)
