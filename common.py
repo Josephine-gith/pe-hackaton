1d03b662841ac0f051427997d91954000fb6cf1c
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2024-10-29 16:36:22 +0100

    update analyse

--- common.py
+++ common.py
@@ -16,6 +16,11 @@ def clean_data(colonne1, colonne2, colonne3 = None):
 def tracage_regression(colonne1, colonne2, colonne3 = None):
     plt.plot(colonne1,colonne2, '+')
     regression = scipy.stats.linregress(colonne1,colonne2)
-    plt.plot(np.linspace(0.4,1,1000), regression[0]*np.linspace(0.4,1,1000)+ regression[1])
+    plt.plot(np.linspace(0.4,1,1000), regression[0]*np.linspace(0.4,1,1000)+ regression[1], label='Régression linéaire')
+    plt.legend(loc='best')
     plt.xlabel(colonne1.name)
     plt.ylabel(colonne2.name)
+
+def analyse(colonne1, colonne2, colonne3 = None):
+    clean = clean_data(colonne1, colonne2)
+    tracage_regression(clean[0], clean[1])
