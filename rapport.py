81b78e8f47ec8307e0423f1557f4db4cbc5def2e
aurelien-bonin <aurelien.bonin@etu.minesparis.psl.eu>
2024-10-29 17:04:55 +0100

    skkkkuuuuuuuurtt

---
+++ rapport.py
@@ -0,0 +1,27 @@
+# # Hackathon P24 #
+#
+# Groupe : MIMOUNI Ines, PERONNE Joséphine, ROMAIN Luigi, BONIN Aurélien 
+#
+# Thème : _Le bonheur_
+#
+# ## Objectifs de l'étude ##
+#
+# Notre objectif ici est de manipuler la base de donnée mise à disposition par le _World happiness report_ afin de mettre
+# certaines affirmations présupposées à l'épreuve des faits.
+#
+# Plus précisement, les hyopthèses que nous avons cherché à confirmer ou infirmer sont les suivantes :
+# - L'argent fait-il le bonheur ?
+# - L'argent fait-il la liberté ?
+# - Le soutien de notre entourage améliore-t-il notre santé ?
+# - Les individus sont-ils plus généreux lorsqu'ils sont bien entourés ?
+# - Les individus sont-ils plus généreux lorsqu'ils ont plus d'opportunités ?
+# - Peut-on à la fois être heureux et malheureux ?
+# - Être bien soutenu par ses proches contribue-t-il à notre sentiment de liberté ?
+#
+# ## Méthodologie ##
+#
+# Pour répondre à ces interrogations, la méthode générale que nous avons adopté est la suivante :
+# - Nettoyer les données à utiliser (suppression des NaN...)
+# - Étudier les liens entre différentes grandeurs (regression linéaires)
+
+
